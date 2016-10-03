bc96b60f88ce0984239c88f851132a93621f2f1d
Lottomax <rasce88@gmail.com>
2016-10-03 10:25:17 -0400

    unversion framework

--- storage/framework/views/5de7eb609a2ce55963c629f12fb9e3873b3a9394.php
+++
@@ -1,5 +0,0 @@
- <!-- BEGIN CONTENT --> 
-
- <!-- div class="col-sm-6 content"> -->
-	<?php echo $__env->yieldContent('content'); ?>   
- <!-- END CONTENT -->  
--- storage/framework/views/df331f95f6ecdd7139f0eb61e3a4a988607d1bfe.php
+++
@@ -1,8 +0,0 @@
- <!-- BEGIN CONTENT --> 
-
- <!-- div class="col-sm-6 content"> -->
-
-	<?php echo $__env->yieldContent('scripts'); ?>
- 
-  
- <!-- END CONTENT -->  
